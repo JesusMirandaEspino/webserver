e002d5a68558392617693c9ae34a419641b89777
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-17 20:49:01 -0600

    csv archive

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ http.createServer( ( req, res ) => {
     res.writeHead( 200, { 'Content-Type': 'application/json' } )
 
 
+   // res.setHeader( 'Content-Disposition', 'attachment; filename=lista.csv' );
+   // res.writeHead( 200, { 'Content-Type': 'application/csv' } )
+
+/*
+    res.write( 'Id', 'Nombre' );
+    res.write( '1', 'Yisus' );
+    res.write( '2', 'Mutuelo' );
+    res.write( '3', 'Json' );
+    res.write( '4', 'Ramo' );
+
+*/
     const persona = {
         id: 1,
         nombre: 'Jesus'
