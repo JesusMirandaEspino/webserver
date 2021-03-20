d7c3f89de40045c2d052d657aa4a99c598b238eb
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-19 20:30:12 -0600

    HBS
    Render con heandler

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//require('hbs');
+app.set('view engine', 'hbs');
+
 //TODO Servir contenido estatico
 
 app.use( express.static( 'public' ) );
 
+
+app.get('/',  (req, res) => {
+    res.render( 'home' );
+});
 
 app.get('/generic',  (req, res) => {
     res.sendFile( __dirname + '/public/generic.html');
